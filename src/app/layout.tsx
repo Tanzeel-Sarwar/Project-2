368e04881a7a067868a55d80f8023cb324fdee88
Tanzeel Sarwar <tanzeelsarwar486@gmail.com>
2025-02-17 21:15:47 +0500

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Navbar />
